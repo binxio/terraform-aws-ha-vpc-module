49b8adb6fb986ea8c88bdbec5aa4d351ca573776
Bruno Schaatsbergen <git@bschaatsbergen.com>
2022-05-05 00:09:55 +0200

    Add `public_subnet_ids` and `private_subnet_ids` to output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "vpc_id" {
   value       = aws_vpc.main.id
   description = "Outputs the unique VPC identifier"
 }
+
+output "public_subnet_ids" {
+  value       = aws_subnet.public_subnet.*.id
+  description = "Outputs the public subnet identifiers"
+}
+
+output "private_subnet_ids" {
+  value       = aws_subnet.private_subnet.*.id
+  description = "Outputs the private subnet identifiers"
+}
